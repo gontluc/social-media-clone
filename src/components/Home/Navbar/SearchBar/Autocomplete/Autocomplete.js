832f98d5356db929a89e1f5bf1b49bfca2eb180f
gontluc <94650197+gontluc@users.noreply.github.com>
2022-06-12 13:58:30 +0200

    Autocomplete

---
+++ src/components/Home/Navbar/SearchBar/Autocomplete/Autocomplete.js
@@ -0,0 +1,11 @@
+import './Autocomplete.css'
+
+const Autocomplete = () => {
+  return (
+    <div className='autocomplete'>
+        Autocomplete
+    </div>
+  )
+}
+
+export default Autocomplete
--- src/components/Home/Navbar/SearchBar/SearchBar.js
+++ src/components/Home/Navbar/SearchBar/SearchBar.js
@@ -2,18 +2,33 @@ import './SearchBar.css'
 import { MdPersonSearch as SearchIcon } from 'react-icons/md'
 import { GoSearch as SearchIconActive } from 'react-icons/go'
 import { useState } from 'react'
+import Autocomplete from './Autocomplete/Autocomplete'
 
 const SearchBar = () => {
     const [activeSearch, setActiveSearch] = useState(false)
+    const [activeAutocomplete, setActiveAutocomplete] = useState(false)
 
   return (
     <div className="searchbar">
         {activeSearch 
-            ? <div className='search-active-div'>
+            ? <div className={
+                `search-active-div ${activeAutocomplete && 'search-active-div-autocomplete'}`
+            }>
                 <SearchIconActive className='search-active'/>
                 <form action="">
-                    <input type="text" placeholder='Search friend' spellCheck={false}/>
+                    <input 
+                        type="text" 
+                        placeholder='Search friend' 
+                        spellCheck={false} 
+                        autoComplete='off'
+                        id='input-text'
+                        onInput={(e) => {
+                            !activeAutocomplete | !e.target.value
+                                && setActiveAutocomplete(!activeAutocomplete)
+                        }}
+                    />
                 </form>
+                {activeAutocomplete && <Autocomplete />}
             </div>
             
             : <SearchIcon 
